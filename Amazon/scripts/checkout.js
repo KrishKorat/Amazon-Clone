0e08d25892ee4cab449b257dba53bc8665509cc2
CelestialMist7 <krishkorat33@gmail.com>
2024-11-10 20:03:45 +0530

    Amazon promise created

--- Amazon/scripts/checkout.js
+++ Amazon/scripts/checkout.js
@@ -2,12 +2,30 @@ import { renderSummaryOrder } from "./checkout/orderSummary.js";
 import { renderPaymentOrder } from "./checkout/paymentSummary.js";
 import { renderCheckoutCount } from "./checkout/checkoutHeader.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
+
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve();
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  })
+]).then(() => {
+  renderSummaryOrder();
+  renderPaymentOrder();
+  renderCheckoutCount();
+});
 
 
-
-
+/*
 loadProducts(() => {
   renderSummaryOrder();
   renderPaymentOrder();
   renderCheckoutCount();
 });
+*/

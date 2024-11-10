b016a2568f2be2d8a3cedd4825afa7d4b3d377c7
CelestialMist7 <krishkorat33@gmail.com>
2024-11-10 19:53:50 +0530

    Amazon backend dummy cart function created

--- Amazon/scripts/checkout.js
+++ Amazon/scripts/checkout.js
@@ -3,6 +3,9 @@ import { renderPaymentOrder } from "./checkout/paymentSummary.js";
 import { renderCheckoutCount } from "./checkout/checkoutHeader.js";
 import { loadAllProducts } from "../data/products.js";
 
+
+
+
 loadAllProducts(() => {
   renderSummaryOrder();
   renderPaymentOrder();
